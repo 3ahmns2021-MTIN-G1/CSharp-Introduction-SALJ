03481b0ea2feeb1cd1548b588d0f7e4f79d8c1ae
Janine Sallmann <janine.sallmann@htl-salzburg.ac.at>
2020-11-09 14:12:31 +0100

    ExampleClass angelegt

---
+++ Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,17 @@
+﻿
+// Bibliotheken / Assembly Referenz / Namespacees
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Deklaration einer Klasse
+
+//public -> access modifier (Zugriffsrechte)
+//class -> keyword zur Deklaration einer Klasse (vorreservierte Schlüsselwörter)
+//ExampleClass -> identifier (Name der Klasse)
+// : MonoBehavior -> Erstellen eine Unterklasse von MonoBehavior
+// {} -> Scope/Wirkungsbereich (was ist alles in der Klasse enthalten)
+public class ExampleClass : MonoBehaviour
+{
+   
+}
